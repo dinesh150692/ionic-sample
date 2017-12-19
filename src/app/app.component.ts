e61eaef15770e6a4893f19408e2f99c0ce03f8b8
Dinesh.Innoventes <dinesh.innoventes@Phonepes-MacBook-Pro.local>
2017-12-19 17:49:14 +0530

    Added new splash screen and config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 
-import { Platform, MenuController, LoadingController, Nav } from 'ionic-angular';
+import { App,Platform, MenuController, LoadingController, AlertController, NavControllerBase} from 'ionic-angular';
 import { LoginPage } from '../pages/login/login';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -11,36 +11,126 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  @ViewChild(Nav) nav: Nav;
+  nav: NavControllerBase;
   rootPage: any = LoginPage;
+  loading: any;
+  alert: any;
 
   constructor(
     public platform: Platform,
     public menu: MenuController,
     public statusBar: StatusBar,
     public splashScreen: SplashScreen,
-    public loadingCtrl: LoadingController
-  ) {
-    this.initializeApp();
+    public loadingCtrl: LoadingController,
+    public zone: NgZone,
+    public alertCtrl: AlertController,
+    public app: App
+  )
+  {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      platform.registerBackButtonAction(() => this.handleHardwareBackButtonPress());
+      platform.resume.subscribe((e) => this.showPause(e));
+      platform.pause.subscribe((e) => this.showResume(e));
+      statusBar.styleDefault();
+      splashScreen.hide();
+      this.zone.run(() =>  {
+        this.initializeApp();        
+      });
+    });
   }
 
   initializeApp() {
-    let loading = this.loadingCtrl.create({
+    this.loading = this.loadingCtrl.create({
       spinner: 'hide',
       content: `<div class="cssload-container">
       <div class="cssload-whirlpool"></div>
-  </div>`
+      </div>`
     });
-  
-    loading.present();
-  
+    this.loading.present();
     setTimeout(() => {
       //this.rootPage = 'LoginPage';
-      loading.dismiss();
-      this.platform.ready().then(() => {
-        this.statusBar.styleDefault();
-        this.splashScreen.hide();
-      });
-    },2000);
+      this.loading.dismiss();
+    },100);
+  }
+
+  handleHardwareBackButtonPress() {
+    this.nav = this.app.getActiveNav();
+    if (this.nav.canGoBack()) {
+      this.nav.pop();
+    } else {
+      this.showExitConfirmation();
+    }
+  }
+
+  showExitConfirmation() {
+    this.alert = this.alertCtrl.create({
+      title: 'Exit?',
+      message: 'Do you want to exit the app?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            this.alert =null;
+          }
+        },
+        {
+          text: 'Exit',
+          handler: () => {
+            this.platform.exitApp();
+          }
+        }
+      ]
+    });
+    this.alert.present();
+  }
+
+  showPause(e: any){
+    this.alert = this.alertCtrl.create({
+      title: 'Exit?',
+      message: 'Do you pause?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            this.alert =null;
+          }
+        },
+        {
+          text: 'Exit',
+          handler: () => {
+            this.platform.exitApp();
+          }
+        }
+      ]
+    });
+    this.alert.present();
+
+  }
+
+  showResume(e: any){
+    this.alert = this.alertCtrl.create({
+      title: 'Exit?',
+      message: 'Do you resume ?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            this.alert =null;
+          }
+        },
+        {
+          text: 'Exit',
+          handler: () => {
+            this.platform.exitApp();
+          }
+        }
+      ]
+    });
+    this.alert.present();
   }
 }
